198521ffb1b619e8bd05e6ffc79557b6897b3cdf
Alec Johnson <alecjohnson55@gmail.com>
2019-11-09 00:27:07 -0800

    fix syntax errors

--- make_calc.py
+++ make_calc.py
@@ -6,8 +6,8 @@ print("")
 print("if 3/2 == 1: # because python 2 does not know maths")
 print("    input = raw_input # python 2 compatibility")
 print("")
-print('welcome to this calculator')
-print('it can add, subtract, and multiply whole numbers')
+print('print("welcome to this calculator")')
+print('print("it can add, subtract, and multiply whole numbers")')
 print("num1 = int(input('enter first number: '))")
 print("sign = input('what operation do you want to do? +, -, /, or *: ')")
 print("num2 = int(input('enter second number: '))")
@@ -21,6 +21,6 @@ for i in range(50):
             try:
                 output = eval(f"{i}{sign}{j}")
                 print(f"if num1 == {i} and sign == '{sign}' and num2 == {j}:")
-                print(f'     print()"{i}"{sign}{j} = {output}")')
+                print(f'     print(\'\"{i}\"{sign}\"{j}\" = \"{output}\"\')')
             except ZeroDivisionError:
                 pass
